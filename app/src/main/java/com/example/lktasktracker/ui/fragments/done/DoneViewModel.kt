16a751d0a3df39a422ad087f2bce84f2d27fcb56
Elizabeth Frolova <lakki00@mail.ru>
2024-10-16 22:19:27 +0300

    Этап 5.1. Модули

--- app/src/main/java/com/example/lktasktracker/ui/fragments/done/DoneViewModel.kt
+++ app/src/main/java/com/example/lktasktracker/ui/fragments/done/DoneViewModel.kt
@@ -1,27 +1,20 @@
 package com.example.lktasktracker.ui.fragments.done
 
-import android.app.Application
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.CreationExtras
-import com.example.lktasktracker.data.TaskDB
 import com.example.lktasktracker.data.TaskModel
 import com.example.lktasktracker.data.TaskRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class DoneViewModel(application: Application) : ViewModel() {
-    private val repository: TaskRepository
+class DoneViewModel @Inject constructor(repository: TaskRepository) : ViewModel() {
     private val _doneTasks = MutableStateFlow<List<TaskModel>>(emptyList())
     val doneTasks = _doneTasks.asStateFlow()
 
     init {
-        val db = TaskDB.getDatabase(application).taskDao()
-        repository = TaskRepository(db)
-
         viewModelScope.launch {
             repository.getCompletedTasks().collect {
                 _doneTasks.value = it
@@ -35,14 +28,14 @@ class DoneViewModel(application: Application) : ViewModel() {
     companion object {
         private val TAG = DoneViewModel::class.java.simpleName
 
-        val APPLICATION_KEY = object : CreationExtras.Key<Application> {}
-        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
-            @Suppress("UNCHECKED_CAST")
-            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
-                val application =
-                    extras[APPLICATION_KEY] as Application
-                return DoneViewModel(application) as T
-            }
-        }
+//        val APPLICATION_KEY = object : CreationExtras.Key<Application> {}
+//        val Factory: ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+//            @Suppress("UNCHECKED_CAST")
+//            override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+//                val application =
+//                    extras[APPLICATION_KEY] as Application
+//                return DoneViewModel(application) as T
+//            }
+//        }
     }
 }
